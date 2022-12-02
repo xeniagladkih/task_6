2c853b74791a294f8e33817a1f9d6f290d63c8b3
Kseniya Gladkih <114312884+xeniagladkih@users.noreply.github.com>
2022-12-02 16:57:02 -0500

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,13 @@
+﻿public class PublishingHouse
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Adress { get; set; }
+}
+
+public class Book
+{
+    public int PublishingHouseId { get; set; }
+    public string? Title { get; set; }
+    public PublishingHouse? PublishingHouse { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+string bookJsonData = File.ReadAllText("Books.json");
+
+var books = JsonSerializer.Deserialize<List<Book>>(bookJsonData);
+
+if (books != null)
+{
+    foreach (var book in books)
+    {
+        Console.WriteLine($"{book.PublishingHouseId} {book.Title} {book.PublishingHouse.Id} {book.PublishingHouse.Name} {book.PublishingHouse.Adress}");
+    }
+}
+
+Console.Read();
